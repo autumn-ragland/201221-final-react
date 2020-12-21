c8e80033dfb50dd0689f470db2096b5454c252ca
autumn-ragland <raglanda998@gmail.com>
2020-12-21 13:14:02 -0600

    add class example

--- src/components/SampleClassBased.jsx
+++ src/components/SampleClassBased.jsx
@@ -5,7 +5,9 @@ class SampleClassBased extends Component {
     constructor(props){
         super(props)
         this.state = {
-            data : []
+            count : 0,
+            data : [],
+            inputText : "Anything"
         }
     }
     componentDidMount(){
@@ -15,10 +17,16 @@ class SampleClassBased extends Component {
         let {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
         this.setState({data : data});
     }
+    
     render(){
         return(
             <>
             <h1>Sample Class Based</h1>
+            <p>Current Count : {this.state.count}</p>
+            <button onClick = {() => this.setState({count : this.state.count + 1})}>Add 1</button>
+            <br/>
+            <input type="text" onChange = {(e) => this.setState({inputText : e.target.value})}/>
+            <p>{this.state.inputText}</p>
             {
                 this.state.data.map((el, idx) => {
                     return <p key = {idx}>{el.name}</p>
