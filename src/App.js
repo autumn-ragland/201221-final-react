3e32ebba321a109a3bb31d7b88ffd195032e2fe2
autumn-ragland <raglanda998@gmail.com>
2020-12-21 13:39:40 -0600

    comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // import logo from './logo.svg';
 // import './App.css';
+
+// import child components from components directory
 import SampleFunctional from './components/SampleFunctional';
 import SampleClassBased from './components/SampleClassBased';
 
 function App() {
   return (
+    // Fragment to group elements
     <>
       <h1>Samples</h1>
+      {/* unused styling */}
       <div className="sampleContainer">
+        {/* render child components */}
         <SampleFunctional />
         <SampleClassBased />
       </div>
--- src/components/SampleFunctional.jsx
+++ src/components/SampleFunctional.jsx
@@ -1,5 +1,5 @@
-import { React, useEffect, useState } from 'react';
-import axios from 'axios';
+import { React, useEffect, useState } from 'react'; // import multiple required dependencies from react
+import axios from 'axios'; // import axios to preform fetch
 
 const SampleFunctional = () => {
     // define hooks with state variables and set state methods with initial value for state variable
@@ -7,22 +7,33 @@ const SampleFunctional = () => {
     const [textDisplay, updateTextDisplay] = useState("Type Anything");
     const [fetchedUsers, setUsers] = useState([]);
 
+    // lifecycle hook with included 2nd parameter to rerun hook only if list changes
     useEffect(() => {
+        // call async method to fetch data from API
         fetchData();
     }, [])
 
+    // declare async method to fetch data and update state
     let fetchData = async () => {
-        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+        // deconstruct response to return the data property
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+        // update state
         setUsers(data);
     }
+    // render
     return (
         <>
             <h1>Sample Functional</h1>
+            {/* display value of count (stateful var) */}
             <p>Current Count : {count}</p>
+            {/* button with event listener to update state */}
             <button onClick={() => updateCount(count + 1)}>Add 1</button>
             <br />
+            {/* text box with event listener to update state */}
             <input type="text" onChange={(e) => updateTextDisplay(e.target.value)} />
+            {/* display the value of textDisplay (stateful var) */}
             <p>{textDisplay}</p>
+            {/* iterate through data returned from fetch */}
             <div>
                 {
                     fetchedUsers.map((el) => {
